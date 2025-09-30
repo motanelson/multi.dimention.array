ae881ec4f759b8c525e7d32dab99b4c79ae7c0c3
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-09-30 22:15:01 +0100

    Add files via upload

--- arrays.py
+++ arrays.py
@@ -1,24 +1,20 @@
 print("\033c\033[43;30m\nboard\n")
-def aarrays(values,c):
-    b=[]
-    for n in range(c):
-        b=b+[values.copy()]
-    return b
-def arrays(arr,values):
-    a=values
-    b=[]
-    c=[]
-    if len(arr)>5:
-        print("array to big")
-        return b
-    for n in range(arr[0]):
-        b=b+[values]
-    c=b.copy()
-    for n in range(1,len(arr)):
-        c=aarrays(c,arr[n])
-    return c
 
-aa=[2,2,2,2]
-a=arrays(aa,0)
+
+print("\033c\033[43;30m\nboard\n")
+
+def arrays(sizes, value):
+    # versão recursiva para criar arrays multidimensionais independentes
+    if len(sizes) == 1:
+        return [value for _ in range(sizes[0])]
+    return [arrays(sizes[1:], value) for _ in range(sizes[0])]
+
+
+# exemplo de uso
+aa = [2,2,2,2]
+a = arrays(aa, 0)
+
+a[0][0][0][0] = 1
+a[0][0][0][1] = 2
 
 print(a)
